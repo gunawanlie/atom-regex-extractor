7c79368619161fefa6d5f04d1ab080cad8412d1d
Gunawan Lie <gunawanlie.toedjoeh@gmail.com>
2020-05-17 19:39:57 +0800

    initial package

---
+++ spec/atom-regex-extractor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomRegexExtractorView from '../lib/atom-regex-extractor-view';
+
+describe('AtomRegexExtractorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
